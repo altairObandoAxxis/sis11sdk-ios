4fde3a21ebc98640eaafdfc6645f8feb49b92e49
Noel Obando <noel.obando@axxis-systems.com>
2024-08-28 23:31:09 +0300

    First version of the redistributable

---
+++ Sis11SDK/Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Sis11SDK",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Sis11SDK",
+            targets: ["Sis11SDK"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "Sis11SDK", path: "./Sources/Sis11SDK.xcframework")
+    ]
+)
